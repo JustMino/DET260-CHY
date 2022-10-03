12ba77d84689e346d6d48839dbba2d144b7c73d1
NoHaxJustMino <chy20a@acu.edu>
2022-10-02 20:14:59 -0500

    working truck and player update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
   [SerializeField] bool onTruck = false;
   [SerializeField] Vector3 vel;
 
+  [SerializeField] Vector3 normalStruck;
+
   private void Start()
   {
       rb = GetComponent<Rigidbody>();
@@ -53,7 +55,7 @@ public class PlayerController : MonoBehaviour
 
   private void FixedUpdate()
   {
-    if (onTruck) rb.velocity = otherrb.velocity;
+    // if (onTruck) rb.velocity = otherrb.velocity;
     MovePlayer();
     vel = rb.velocity;
   }
@@ -111,7 +113,6 @@ public class PlayerController : MonoBehaviour
   {
     GetComponent<HingeJoint>().autoConfigureConnectedAnchor = false;
     Destroy (GetComponent<HingeJoint>());
-    // rb.mass = 1;
     rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
 
   }
@@ -124,30 +125,29 @@ public class PlayerController : MonoBehaviour
   {
     if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
     {
-      otherrb = other.gameObject.GetComponent<Rigidbody>();
       grounded = true;
-      // onTruck = true;
-      var hj = gameObject.AddComponent<HingeJoint>();
-      GetComponent<HingeJoint>().connectedBody = otherrb;
-      rb.mass = 0.00001f;
-      // rb.isKinematic = true;
-      // GetComponent<Collider>().material.bounciness = 0;
-      // rb.freezeRotation = true;
-      // rb.velocity = Vector3.zero;
+      ContactPoint otherContact = other.contacts[0];
+      normalStruck = otherContact.normal;
+      if (normalStruck.y > 0.9f)
+      {
+        onTruck = true;
+        otherrb = other.gameObject.GetComponent<Rigidbody>();
+        var hj = gameObject.AddComponent<HingeJoint>();
+        GetComponent<HingeJoint>().connectedBody = otherrb;
+        rb.mass = 0.00001f;
+      }
     }
   }
 
-  void OnCollisionExit(Collision other)
+  void OnTriggerExit(Collider other)
   {
     if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
     {
       otherrb = null;
       grounded = false;
       onTruck = false;
-      // transform.parent = null;
       Destroy (GetComponent<HingeJoint>());
       rb.mass = 0.01f;
-      // rb.isKinematic = false;
     }
   }
 }
