28b535a9d881eea6b31c47d9a83d32e4455b04d5
NoHaxJustMino <cyamaguchi.mino@gmail.com>
2022-10-06 10:03:04 -0500

    last save before testing URP

--- Assets/Scripts/ResetLevel.cs
+++ Assets/Scripts/ResetLevel.cs
@@ -9,7 +9,7 @@ public class ResetLevel : MonoBehaviour
     {
       if (other.gameObject.tag == "Player")
       {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
       }
     }
 }
