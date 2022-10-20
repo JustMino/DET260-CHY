615149afc9e983011f18c3485dc15800c5eacdac
NoHaxJustMino <chy20a@acu.edu>
2022-10-20 08:48:51 -0500

    punch anim

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+  [Header("Needed Components")]
+  Animator anim;
+
+  [Header("Player Parameters")]
+  int atkcount = 0;
+  bool m_Started;
+  bool canattack = true;
+  [SerializeField] LayerMask enemylayers;
+
+  [System.Serializable]
+  public class lightparam
+  {
+    public int dmg = 10;
+    public GameObject atkPoint;
+    public Vector3 hitboxsize = new Vector3 (1.0f, 1.0f, 1.0f);
+    public Vector3 t;
+  }
+
+  public lightparam LightParam = new lightparam();
+  // Start is called before the first frame update
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+    LightParam.atkPoint = GameObject.Find("Hitbox");
+    m_Started = true;
+    LightParam.t = LightParam.atkPoint.transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetButtonDown("Light") && canattack)
+    {
+      canattack = false;
+      LightParam.t.x *= -1f;
+      LightParam.atkPoint.transform.position = LightParam.t;
+      anim.SetBool("Side(True=Right)", !anim.GetBool("Side(True=Right)"));
+      anim.SetTrigger("Punch");
+    }
+  }
+
+  public void LightHitCheck()
+  {
+    Collider[] hit = Physics.OverlapBox(LightParam.atkPoint.transform.position, LightParam.hitboxsize, transform.rotation, enemylayers);
+    foreach(var hitCollider in hit)
+    {
+      EnemyHealth enemyhealth = hitCollider.GetComponent<EnemyHealth>();
+      enemyhealth.Damage(LightParam.dmg);
+    }
+  }
+
+  public void resetatkcooldown()
+  {
+    canattack = true;
+  }
+
+  void OnDrawGizmos()
+  {
+    Gizmos.color = Color.red;
+    if (m_Started) Gizmos.DrawWireCube(LightParam.atkPoint.transform.position, LightParam.hitboxsize);
+  }
+}
