cc6dac3241871faf62d1e162bc8de08099aa7aa7
NoHaxJustMino <57928928+JustMino@users.noreply.github.com>
2022-11-15 10:50:25 -0600

    added Jump animation

--- Assets/Scripts/UpdatePlayerParams.cs
+++ Assets/Scripts/UpdatePlayerParams.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class UpdatePlayerParams : MonoBehaviour
 {
   Animator anim;
+  PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
       anim = GetComponent<Animator>();
+      player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -17,5 +19,6 @@ public class UpdatePlayerParams : MonoBehaviour
         anim.SetInteger("HorizontalInput", (int)Input.GetAxisRaw("Horizontal"));
         anim.SetInteger("VerticalInput", (int)Input.GetAxisRaw("Vertical"));
         anim.SetBool("Sprinting", (Input.GetButton("Sprint")) ? true : false);
+        if (player.grounded && Input.GetButtonDown("Jump")) anim.SetTrigger("Jump");
     }
 }
